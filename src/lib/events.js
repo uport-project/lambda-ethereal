a1f22e0abb6a1d0388077b81bdbec38c97e0ae1f
KamesCG <kamescg@gmail.com>
2018-03-21 09:21:48 -0400

    Added uPort Community Call #3 to events.js

--- src/lib/events.js
+++ src/lib/events.js
@@ -70,5 +70,17 @@ module.exports = {
         },
         signer_name: "uPort Team",
         signer_mnid: '2oiSESCx5Y7oMHDAxx9iUBkz88rdA9yvmW3'  
+    },
+    uportCommunityCall3: {
+        expire: 1522080300, // Monday, March 26, 2018 12:05:00 PM GMT-04:00
+        claim: {
+            "Attended uPort Community Call": {
+                event: "uPort Community Call #3",
+                date: "March 26, 2018",
+                location: "uPort Town Hall"
+            }
+        },
+        signer_name: "uPort Team",
+        signer_mnid: '2oiSESCx5Y7oMHDAxx9iUBkz88rdA9yvmW3'  
     }
 }
