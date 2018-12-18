f2eabf18a5b98d0d45e5bfb5f5678f7fae7df667
Andres Junge <ajunge@yaykuy.cl>
2018-12-18 10:11:13 -0300

    comm call 2018-12

--- src/lib/events.js
+++ src/lib/events.js
@@ -10,6 +10,17 @@ module.exports = {
     signer_name: "uPort Team",
     signer_mnid: "2oiSESCx5Y7oMHDAxx9iUBkz88rdA9yvmW3"
   },
+  communityCall201812: {
+    expire: 1545153300, //Tue, 18 Dec 2018 17:15:00 +0000
+    claim: {
+      "Attended uPort Community Call Dec-2018": {
+        event: "uPort Community Call",
+        date: "2018-12-18"
+      }
+    },
+    signer_name: "uPort Team",
+    signer_mnid: "2oiSESCx5Y7oMHDAxx9iUBkz88rdA9yvmW3"
+  },
   meshCredential: {
     expire: 32503680000, //Wed, 01 Jan 3000 00:00:00 +0000 
     claim: {
